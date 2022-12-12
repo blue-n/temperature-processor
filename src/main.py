9f9655000bd72d417a6a35a56e399db28af3851b
Nardine Eskander <nardineeskander@Nardines-MBP.cgocable.net>
2022-12-12 09:11:44 -0500

    This is the pytest testing all the functions

--- src/main.py
+++ src/main.py
@@ -12,33 +12,6 @@ print("Timestamp is:", ts)
 app = Flask(__name__)
 
 #-----------------------------------------#
-
-""" def main():
-    with open('sensor.json', 'r') as f:
-        data = json.load(f)
-
-    for item in data:
-        for key, value in item.items():
-            # print(item)
-            # print(item["laundryRoom"])
-
-            if key == "laundryRoom":
-                print("Laundry Room: ", value)
-            elif key == "temperature":
-                temperature(value)
-                #print("Temperature: ", value)
-            elif key == "humidity":
-                # What it might look like to factor in other logic than just the read  & print
-                humidity(value)
-            elif key == "smokeAlarm":
-                print("Smoke Alarm: ", value)
-            elif key == "fireAlarm":
-                print("Fire Alarm: ", value)
-            elif key == "message":
-                message(value)
-            else:
-                print(key) """
-
 @app.route("/metric/<key>/<enter>")
 def processor(key, enter):
     if key == "laundryRoom":
--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -10,5 +10,29 @@ def test_firealarm_no_fire():
 # assert <functionname>.<functionname>(<parameters>) == <expected result>
 
 #Example
-#def test_humidity():
-#    assert humidity.humidity("10%") == .1
+def test_humidity_positive():
+    assert humidity.humidity("10%") == .1
+
+def test_humidity_negative():
+    assert humidity.humidity("-5%") == -0.05
+
+def test_laundryroom_positive():
+    assert laundryroom.laundryRoom("100") == "100"
+
+def test_laundryroom_words():
+    assert laundryroom.laundryRoom("berry") == "berry"
+
+def test_smokealarm_True():
+    assert smokealarm.smokeAlarm("smoke") == True
+
+def test_smokealarm_False():
+    assert smokealarm.smokeAlarm("No smoke") == False
+
+def test_temperature_positive():
+    assert temperature.temperature("25") == 25
+
+def test_temperature_negative():
+    assert temperature.temperature("-535") == -535
+
+def test_message_words():
+    assert message.message("everything is working") == "everything is working"
